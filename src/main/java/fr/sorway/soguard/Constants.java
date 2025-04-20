e2587fcc8831f69f4d3e3ed131f3cf601d1b7d30
Sorway <sorwayhd@gmail.com>
2025-04-21 00:34:07 +0200

    📣 Adding feed

--- src/main/java/fr/sorway/soguard/Constants.java
+++ src/main/java/fr/sorway/soguard/Constants.java
@@ -3,8 +3,11 @@ package fr.sorway.soguard;
 import net.dv8tion.jda.api.Permission;
 
 public class Constants {
-    public static final String ADVISORIES_URL = "https://www.cisecurity.org/feed/advisories";
-    public static final String ANSSI_ALERT = "https://www.cert.ssi.gouv.fr/feed/";
+    public static final String[] FEEDS = new String[]{
+            "https://www.cert.ssi.gouv.fr/feed/",
+            "https://filestore.fortinet.com/fortiguard/rss/outbreakalert.xml",
+            "https://www.cisecurity.org/feed/advisories"
+    };
     public static final Permission[] DEFAULT_BOT_PERMISSIONS = new Permission[] {
             Permission.MESSAGE_SEND,
             Permission.MESSAGE_EMBED_LINKS,
